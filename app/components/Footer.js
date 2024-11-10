112972b2b7a2cab27a3e08edaea00d58cd919b7a
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-10 10:30:10 +0100

    Add Footer component and integrate it into the layout

---
+++ app/components/Footer.js
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>
+        Made by{" "}
+        <a href="mailto:race@eaaa.dk" target="_blank">
+          RACE
+        </a>
+      </p>
+    </footer>
+  );
+}
