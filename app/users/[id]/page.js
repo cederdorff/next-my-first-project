6bb5d62dc39a64a90e06f4e65f8de7af13dbf125
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-10 20:07:28 +0100

    Fetch user details from Firebase and display user information on user detail page; update user list with dynamic links

--- app/users/[id]/page.js
+++ app/users/[id]/page.js
@@ -1,13 +1,28 @@
+import Image from "next/image";
 import styles from "../../page.module.css";
 export default async function UserDetail({ params }) {
   const { id } = await params;
 
-  console.log(id);
+  const response = await fetch(
+    `https://fb-rest-race-default-rtdb.firebaseio.com/users/${id}.json`
+  );
+  const user = await response.json();
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>User Detail Page for ID: {id}</h1>
+        <h1>{user.name}</h1>
+        <Image
+          src={user.image}
+          width="300"
+          height="400"
+          alt={user.name}
+          className="user-image"
+        />
+        <p>{user.title}</p>
+        <p>
+          <a href={`mailto:${user.mail}`}>{user.mail}</a>
+        </p>
       </main>
     </div>
   );
--- app/users/page.js
+++ app/users/page.js
@@ -1,21 +1,30 @@
 import Link from "next/link";
 import styles from "./../page.module.css";
-export default function Users() {
+
+export default async function Users() {
+  const response = await fetch(
+    "https://fb-rest-race-default-rtdb.firebaseio.com/users.json"
+  );
+  const dataObject = await response.json();
+  const users = Object.keys(dataObject).map(key => {
+    return {
+      id: key,
+      ...dataObject[key]
+    };
+  });
+
+  console.log(users);
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <h1>Users</h1>
         <p>Here is a list of users.</p>
         <ol>
-          <li>
-            <Link href="/users/1">John Doe</Link>
-          </li>
-          <li>
-            <Link href="/users/2">Jane Doe</Link>
-          </li>
-          <li>
-            <Link href="/users/3">Foo Bar</Link>
-          </li>
+          {users.map(user => (
+            <li key={user.id}>
+              <Link href={`/users/${user.id}`}>{user.name}</Link>
+            </li>
+          ))}
         </ol>
       </main>
     </div>
