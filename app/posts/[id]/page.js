b930da79ad7c14482da4d292ff5eda0b77adfaf5
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2024-11-10 19:07:36 +0100

    Fetch post details from Firebase and display image on post detail page; add global styles for post images

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,13 +1,26 @@
+import Image from "next/image";
 import styles from "../../page.module.css";
 export default async function PostDetail({ params }) {
   const { id } = await params;
-
   console.log(id);
+
+  const response = await fetch(
+    `https://fb-rest-race-default-rtdb.firebaseio.com/posts/${id}.json`
+  ); // Fetch data from Firebase Realtime Database
+  const post = await response.json(); // Get the JSON data from the response
+  console.log(post);
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Post Detail Page for ID: {id}</h1>
+        <h1>{post.caption}</h1>
+        <Image
+          src={post.image}
+          width="300"
+          height="400"
+          alt={post.caption}
+          className="post-image"
+        />
       </main>
     </div>
   );
