4b8ad287fee28b30c0afb9bcb18d1551848867ac
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2025-11-10 13:39:51 +0100

    Refactor: Move Header, Footer, and Navbar components to the components directory

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
